2d037a8c41d5d4fe7b56c7be405b91f29ab12a35
hvg-cyber <hvgvf2345@gmail.com>
2026-02-05 13:20:41 +0300

    Add tect/joxan/vslib/worker/0notes.js via GitHub Folder Uploader

---
+++ tect/joxan/vslib/worker/0notes.js
@@ -0,0 +1,23 @@
+/*
+
+Main
+sw.js
+webWorkerExtensionHostIframe.html
+ExtensionHostWorker
+ExtensionHostWorker -> anycode.server.browser.js
+
+***** When a file is opened *****
+
+editorWorkerService
+LanguageDetectionWorker
+
+***** When a directory is opened *****
+
+LocalFileSearchWorker
+TextMateWorker
+ExtensionHostWorker -> jsonServerMain.js
+
+
+
+
+*/
