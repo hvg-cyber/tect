ad24f6257bc249cd70e7f80ee1a6704b80485076
hvg-cyber <hvgvf2345@gmail.com>
2026-02-05 13:14:00 +0300

    Add tect/sysprog/multiple_examples/example1.c via GitHub Folder Uploader

---
+++ tect/sysprog/multiple_examples/example1.c
@@ -0,0 +1,22 @@
+/*
+This file contains multiple examples
+with one main() function
+*/
+#include <stdio.h>
+
+void test1() {
+    printf("Test 1\n");
+}
+
+void test() {
+    printf("Test 2\n");
+}
+
+void test3() {
+    printf("Test 3\n");
+}
+
+int main() {
+    test();
+    return 0;
+}
