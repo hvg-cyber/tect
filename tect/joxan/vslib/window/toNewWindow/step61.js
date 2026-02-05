1ce8692f4f215f1650e1fc82751fb6e5696bb05b
hvg-cyber <hvgvf2345@gmail.com>
2026-02-05 13:20:46 +0300

    Add tect/joxan/vslib/window/toNewWindow/step61.js via GitHub Folder Uploader

---
+++ tect/joxan/vslib/window/toNewWindow/step61.js
@@ -0,0 +1,16 @@
+/*
+
+This program demonstrates how the following commands are implemented:
+workbench.action.moveEditorToNewWindow
+workbench.action.moveEditorGroupToNewWindow
+workbench.action.copyEditorToNewWindow
+workbench.action.copyEditorGroupToNewWindow
+
+
+Bug: 'workbench.action.moveEditorToNewWindow'
+moves the stuff into an auxiliary window.
+This does not seem right.
+And it does not work in web at all.
+
+
+*/
