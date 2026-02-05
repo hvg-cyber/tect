1d8093fb014751eb42160f0f40149bf0ad36e5a5
hvg-cyber <hvgvf2345@gmail.com>
2026-02-05 13:20:59 +0300

    Add tect/joxan/vslib/voice_recorder/step0.js via GitHub Folder Uploader

---
+++ tect/joxan/vslib/voice_recorder/step0.js
@@ -0,0 +1,9 @@
+/*
+
+There are 2 settings available:
+accessibility.signals.voiceRecordingStarted
+accessibility.signals.voiceRecordingStopped
+
+This suggests that I can get access to the mic and record audio.
+How do I do that?
+*/
