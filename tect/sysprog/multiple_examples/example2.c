da9f808528b46a2059a416e0eab9fb27127b5ffb
hvg-cyber <hvgvf2345@gmail.com>
2026-02-05 13:14:01 +0300

    Add tect/sysprog/multiple_examples/example2.c via GitHub Folder Uploader

---
+++ tect/sysprog/multiple_examples/example2.c
@@ -0,0 +1,26 @@
+/*
+This file contains multiple examples
+using preprocessor directives
+*/
+#include <stdio.h>
+
+#if 1
+int main() {
+    printf("Test 11\n");
+    return 0;
+}
+#endif
+
+#if 0
+int main() {
+    printf("Test 22\n");
+    return 0;
+}
+#endif
+
+#if 0
+int main() {
+    printf("Test 33\n");
+    return 0;
+}
+#endif
