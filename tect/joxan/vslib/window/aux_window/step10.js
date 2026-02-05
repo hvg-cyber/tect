3647def5df7038f3593281230fe7cc73fc347c5d
hvg-cyber <hvgvf2345@gmail.com>
2026-02-05 13:20:53 +0300

    Add tect/joxan/vslib/window/aux_window/step10.js via GitHub Folder Uploader

---
+++ tect/joxan/vslib/window/aux_window/step10.js
@@ -0,0 +1,19 @@
+/*
+    Add more features
+*/
+console.log('Step 10')
+
+const features = [
+    'popup=yes',
+    `left=100`,
+    `top=300`,
+    `width=300`,
+    `height=100`,
+]
+const auxiliaryWindow = window.open(
+    'about:blank',
+    undefined,
+    features.join(',')
+)
+document.body.textContent = 'Main window'
+auxiliaryWindow.document.body.textContent = 'Aux window'
