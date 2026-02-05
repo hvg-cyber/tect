b4e8634070b5ed73049b0612ed8de59bde7f5498
hvg-cyber <hvgvf2345@gmail.com>
2026-02-05 13:13:52 +0300

    Add tect/sysprog/info.c via GitHub Folder Uploader

---
+++ tect/sysprog/info.c
@@ -0,0 +1,18 @@
+/*
+********** Info **********
+
+https://en.cppreference.com/w/c/language.html
+
+
+
+********** install gcc **********
+
+$ sudo apt install gcc
+
+********** How to compile programs **********
+
+$ gcc hello.c -o hello
+$ ./hello
+
+
+*/
